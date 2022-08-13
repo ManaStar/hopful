086ff4d67afd36cca605076af71a80b3a883180f
Mana Kampman <manastar04@gmail.com>
2022-08-12 18:33:34 -0700

    Things are ready to go

---
+++ src/main/java/com/mitebemodding/hopful/core/Hopful.java
@@ -0,0 +1,14 @@
+package com.mitebemodding.hopful.core;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hopful implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Hopful");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+	}
+}
